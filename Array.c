461da42234d6a499e3013721c16685f1558f783e
Palash Saha <61386800+mrpalashsaha@users.noreply.github.com>
2020-06-10 17:54:12 +0300

    Update Array.c

--- Array.c
+++ Array.c
@@ -1,4 +1,69 @@
 # include <stdio.h>
 # include <
+#defiine MIN_VALUE '1'
+# define MAX_VALUE '9'
+#define EMPTY_VALUE '-'
+#define NUMVALUES (MAX_VSLUE-MIN_VALUE +1)
+#define GRID_SIZE ( NUM_VALUES * NUM_VALUES)
+#define MAX_CELL (GRID_SEIZE - 1)
+typedef char value;
+typedef int cell;
+typedef sudukuGrid values [GRID_SIZE);
+int hasSolution (sudukuGrid game);
 
-//& x = address of x 
+int main (int argc, const char *argv []) {
+  
+  suduKuGrid sample ;
+  readGame (sample);
+  
+  //showGame (sample );
+  
+  if (hasSolution(sample)){
+    showGame (sample);
+  
+}
+  
+  return 0;
+  
+}
+                           
+int hasSolution (sudukuGrid game){
+  INT solved;
+  cell candidateCell;
+  value trialValue
+  if (isFull (game)) {
+    solved = TRUE;
+  } else {
+    candidateCell = getEmptyCell ( game) ;
+    trialvalue = MIN_VALUE;
+    solved = FALSE;
+    
+   while ( !solved && trialValue <= MAXVALUE)) {
+       if (isLegal (game, candidateCell, trialValule)) 
+       setCell (game, candidateCell, trialValue);
+        if (hasSolution (game)) {
+        solved = TRUE;
+   }else {
+          clearCell (game, candidateCell);
+        }
+  }
+    trialValue =;
+  }
+}
+  }
+                           
+    
+  return solved;
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+                           
+           
