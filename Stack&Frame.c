ad300d5a08dde70a9b5250651c2fc737d67e76e2
Palash Saha <61386800+mrpalashsaha@users.noreply.github.com>
2020-06-11 05:21:36 +0300

    Create Stack&Frame.c

---
+++ Stack&Frame.c
@@ -0,0 +1,2 @@
+// add new item on the top and take the item from the top is called Stack. stack pointer keep track the function and wehn it is finish it return the pointer to down 
+//stck overflow means somehow you have a infinite loop
