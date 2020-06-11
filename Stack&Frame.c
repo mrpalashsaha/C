58ceacb39cf93ad5c9eabff83bfeca0332d621c7
Palash Saha <61386800+mrpalashsaha@users.noreply.github.com>
2020-06-11 16:14:03 +0300

    Update Stack&Frame.c

--- Stack&Frame.c
+++ Stack&Frame.c
@@ -1,2 +1,23 @@
 // add new item on the top and take the item from the top is called Stack. stack pointer keep track the function and wehn it is finish it return the pointer to down 
 //stck overflow means somehow you have a infinite loop
+// xtreme programming: is me
+// waterfall method: gather information, planning, prototye, write your code and testing and basck to again
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;
+ 
