7c62683d88ad0aacb08b7dd7c6d5983444f302cc
Palash Saha <61386800+mrpalashsaha@users.noreply.github.com>
2020-06-12 17:25:18 +0300

    Create commonValue.c

---
+++ commonValue.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+static void mostCommonValue ()
+value mostCommonValue (sudukuGrid game);
+
+// return most common digit in the grid if two are equally common return either so, if all empty return any
+
+static void mostCommonValue () {
+sudukuGrid; 
+sudukuGrid fullGrid = {'1', '2', '3', '4', '5', '6', '7', '8', '9',
+                        '1', '2', '3', '4', '5', '6', '7', '8', '9'
+                        '1', '2', '3', '4', '5', '6', '7', '8', '9'
+                        '1', '2', '3', '4', '5', '6', '7', '8', '9'
+                        '1', '2', '3', '4', '5', '6', '7', '8', '9'
+                        '1', '2', '3', '4', '5', '6', '7', '8', '9'
+                        '1', '2', '3', '4', '5', '6', '7', '8', '9'
+                        '1', '2', '3', '4', '5', '6', '7', '8', '9'
+                        '1', '2', '3', '4', '5', '6', '7', '8', '9'
+}; 
+clearGame(grid);
+value common;
+printf("testing most common value ");
+common = mostCommonValue (grid);
+assert (common <= MAX_VALUE );
+assert (common >= MIN_VALUE);
+
+//test returns val for full grid
+
+common  mostCommonValue (fullGrid);
+assert (common <= MAX_VALUE );
+assert (common >= MIN_VALUE);
+
+void mostCommonValue (sudukuGrid grid) {
+  int count [NUM_VALUES]= {O};
+  value 
+  while (possibleValue <= MAX_VAUE) {
+
+
+
+
+}
