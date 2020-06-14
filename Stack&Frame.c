6a4cfcfd5289e931985bfc6a51ec5d93a30051b9
Palash Saha <61386800+mrpalashsaha@users.noreply.github.com>
2020-06-14 06:36:45 +0300

    Update Stack&Frame.c

--- Stack&Frame.c
+++ Stack&Frame.c
@@ -1,9 +1,14 @@
 // add new item on the top and take the item from the top is called Stack. stack pointer keep track the function and wehn it is finish it return the pointer to down 
-//stck overflow means somehow you have a infinite loop
-// xtreme programming: is me
+//stack overflow means somehow you have a infinite loop
+// xtreme programming: 
 // waterfall method: gather information, planning, prototye, write your code and testing and basck to again
+// when you call the function struct send the copy but arrray send the reference
 
-
+struct sudukuGri { 
+ valuecontent [grid_size];
+ int emptyCellCount;
+}
+type def struct sudukuGri sudukuGrid 
 
 
 
